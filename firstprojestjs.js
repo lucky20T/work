2726f4891c9e693fd4d0b37127b8bd913c0ffa8c
Vishal Chakradhari <vishalchakradhari2005@gmail.com>
2025-03-03 19:10:03 +0530

    Add files via upload

---
+++ firstprojestjs.js
@@ -0,0 +1,5 @@
+document.getElementById('search-btn').addEventListener('click', () => {
+    const query = document.getElementById('search-bar').value;
+    console.log(`Searching for: ${query}`);
+    // Make an API call to fetch search results
+});
